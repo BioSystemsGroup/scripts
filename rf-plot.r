7b65f87e3ed0bce24084c9e3848a31e0d1f09001
gepr <gepr@zareason-mediabox5330>
2017-05-10 15:15:50 -0700

    • plot both dPV and dCV

--- rf-plot.r
+++ rf-plot.r
@@ -4,7 +4,7 @@
 ##
 # Read multiple reaction field MC trial data files from the experiment directory and plot them as images.
 #
-# Time-stamp: <2017-05-08 15:52:22 gepr>
+# Time-stamp: <2017-05-10 15:10:35 gepr>
 #
 #dev.off()
 
@@ -18,7 +18,7 @@ usage <- function() {
 }
 if (length(argv) < 1) usage();
 
-require(imager)
+suppressMessages(library(imager))
 source("~/R/misc.r")
 
 ##
@@ -28,38 +28,47 @@ if (!file.exists("graphics")) dir.create("graphics")
 
 exp <- argv[1]
 
-## test for the averaged file
-filename <- paste(exp,"rxnprod",sep="_")
-if (!file.exists(paste(filename,".csv",sep=""))) usage()
+for (dir in c("dPV","dCV")) {
+  ## test for the averaged file
+  filename <- paste(exp,"_rxnprod-",dir,sep="")
+  if (!file.exists(paste(filename,".csv",sep=""))) usage()
 
-pdf(paste("graphics/",filename,".pdf",sep=""),width=8.5,height=11)
-par(mar=c(5,6,4,2), oma=c(0,0,3,0), cex.main=2, cex.axis=2, cex.lab=2)
-rf <- read.csv(paste(filename,".csv",sep=""))
-rfi <- as.cimg(t(as.matrix(rf)))
-plot(rfi,asp="varying",main="Average", xlab="dPV", ylab="Cycle")
-grid()
-minor.tick(nx=5,ny=5)
+  print(paste("Generating avg ",dir," rxn field for ",exp,sep=""))
 
-###
-## loop through MC trial files and add them to the PDF
-###
-files <- list.files(path=exp, pattern="rxnprod-[0-9]+.csv",recursive=T,full.names=T)
-nplots <- length(files)
-ppp <- 4
-
-par(mfrow=c(ppp,1))
-trials <- 1
-for (f in files) {
-  rf <- read.csv(f,colClasses="numeric")
+  pdf(paste("graphics/",filename,".pdf",sep=""),width=8.5,height=11)
+  par(mar=c(5,6,4,2), oma=c(0,0,3,0), cex.main=2, cex.axis=2, cex.lab=2)
+  rf <- read.csv(paste(filename,".csv",sep=""))
   rfi <- as.cimg(t(as.matrix(rf)))
-  plot(rfi,asp="varying",main=paste("Trial",trials), xlab="dPV",ylab="Cycle")
+  plot(rfi,asp="varying",main="Average", xlab=dir, ylab="Cycle")
   grid()
   minor.tick(nx=5,ny=5)
-  ##if (trials %% ppp == 0) par(mfrow=c(ppp,1))
-  trials <- trials + 1
-}
-title(paste(exp,"dPV Reaction Fields"),outer=T)
-dev.off()
+
+  ###
+  ## loop through MC trial files and add them to the PDF
+  ###
+  pattern <- paste("rxnprod-",dir,"-[0-9]+.csv",sep="")
+  files <- list.files(path=exp, pattern=pattern,recursive=T,full.names=T)
+  nplots <- length(files)
+  ppp <- 4
+
+  par(mfrow=c(ppp,1))
+  trials <- 1
+  for (f in files) {
+
+    print(paste("    Now trial ",trials,sep=""))
+
+    rf <- read.csv(f,colClasses="numeric")
+    rfi <- as.cimg(t(as.matrix(rf)))
+    plot(rfi,asp="varying",main=paste("Trial",trials), xlab=dir,ylab="Cycle")
+    grid()
+    minor.tick(nx=5,ny=5)
+    ##if (trials %% ppp == 0) par(mfrow=c(ppp,1))
+    trials <- trials + 1
+  }
+  title(paste(exp,dir,"Reaction Fields"),outer=T)
+  dev.off()
+
+} ## end loop over direction
 
 ##> rf0 <- read.csv("ecrf000x/rxnprod-0000.csv.gz")
 ##> rf0i <- as.cimg(t(as.matrix(rf0)))
