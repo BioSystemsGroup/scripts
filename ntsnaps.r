85b913c0950d3a70b75a67c2d67129635b1ed312
gepr <gepr@tempusdictum.com>
2018-04-05 11:25:08 -0700

    • Add ANIMATE parameter. • Change to grayscale.

--- ntsnaps.r
+++ ntsnaps.r
@@ -2,9 +2,10 @@
 ###
 ## Read multiple reaction field MC trial data files from the experiment directory and plot them as images.
 ##
-## Time-stamp: <2018-04-03 17:55:15 gepr>
+## Time-stamp: <2018-04-05 11:16:02 gepr>
 ###
 
+ANIMATE <- T
 NUM.STATES <- 3
 
 argv <- commandArgs(T)
@@ -31,9 +32,9 @@ for (snap in argv) {
   cycle <- substr(snap,regexpr('-[0-9]+.csv',snap)+1,regexpr('.csv',snap)-1)
   s <- read.csv(snap)
   ## divide by number of states to get a range of one color (blue) and render as a data.frame
-  sxyv <- as.data.frame(as.cimg(as.vector(as.matrix(s[,2:ncol(s)]))/NUM.STATES,x=ncol(s)-1,y=length(s[,1])))
+  sxyv <- as.data.frame(as.cimg(as.vector(t(as.matrix(s[,2:ncol(s)])))/NUM.STATES,x=ncol(s)-1,y=length(s[,1])))
   ## append the color column
-  sxyvc <- cbind(sxyv,rep(3,length(sxyv)))
+  sxyvc <- cbind(sxyv,rep(1,nrow(sxyv)))
   colnames(sxyvc) <- c(colnames(sxyv),"cc")
   ## reorder the columns for as.cimg()
   sxycv <- sxyvc[,c("x","y","cc","value")]
@@ -41,11 +42,13 @@ for (snap in argv) {
   i <- as.cimg(sxycv)
 
   imFile <-  paste(filebase,"-",cycle,".png", sep="")
-  png(imFile,width=ncol(s),height=nrow(s))
-  par(mar=rep(0,4)) ## no margins!
-  plot(as.cimg(i),interpolate=F,colourscale=cscale,rescale=F,axes=F)
-  dev.off()
-
+  if (F) {
+    png(imFile,width=ncol(s)-1,height=nrow(s))
+    par(mar=rep(0,4)) ## no margins!
+    plot(i,interpolate=F,colorscale=cscale,rescale=F,axes=F)
+    dev.off()
+  }
+  if (T) save.image(i,imFile)
   ## use ImageMagick to scale it up
   scaleX <- ncol(s)*20
   scaleY <- nrow(s)*20
@@ -55,8 +58,9 @@ for (snap in argv) {
 }
 
 ## animate the images
-snapStart <- regexpr('/snap',filebase)
-snapStop <- regexpr('-[0-9]+-SS',filebase)
-animFileName <- paste(substr(filebase,0,snapStart),"anim",substr(filebase, snapStop,nchar(filebase)),".gif", sep="")
-system(paste("convert -delay 1 ",pngs," ", animFileName, sep=""))
-
+if (ANIMATE) {
+  snapStart <- regexpr('/snap',filebase)
+  snapStop <- regexpr('-[0-9]+-SS',filebase)
+  animFileName <- paste(substr(filebase,0,snapStart),"anim",substr(filebase, snapStop,nchar(filebase)),".gif", sep="")
+  system(paste("convert -delay 1 ",pngs," ", animFileName, sep=""))
+}
