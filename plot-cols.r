2c6e8736681bf6f58f4d5dcc38dfc86d20da833c
gepr <gepr@tempusdictum.com>
2019-09-09 10:47:07 -0700

    • Bring up to speed with cmp-plot.r improvements like wrapping the source() command, adding <raw|data|nodata> option, etc.

--- plot-cols.r
+++ plot-cols.r
@@ -3,39 +3,63 @@
 ###
 ## Read multiple *.csv files and plot each column.
 ##
-## Time-stamp: <2018-05-22 11:19:26 gepr>
+## Time-stamp: <2019-09-09 10:14:11 gepr>
 ##
 
+sample.freq <- 10
+ma.window <- 181
 plot.data <- F
-plot.svg <- F
+plot.svg <- T
 
 source("~/R/misc.r") # for moving average
 
 argv <- commandArgs(TRUE)
 
-if (length(argv) < 1) {
-    print("Usage: plot-cols <CSV file1> <CSV file2>")
-    print("  e.g. plot-cols.r exp_hsolute-dCV∈[0,100).csv")
-    quit()
+usage <- function() {
+  print("Usage: plot-cols <raw|data|nodata> <CSV file1> <CSV file2>")
+  print("  e.g. plot-cols.r exp_hsolute-dCV∈[0,100).csv")
+  quit()
 }
+if (length(argv) < 2) {
+  cat( "Not enough arguments.\n" )
+  usage()
+}
+data.status <- argv[1]
+if (data.status != "raw" && data.status != "data" && data.status != "nodata") {
+  cat( paste("Data status", data.status, "invalid.\n"))
+  usage()
+}
+
+files <- argv[-1]
 
 ###
 ## test for and create graphics subdirectory
 ###
 if (!file.exists("graphics")) dir.create("graphics")
 
-for (f in argv) {
+for (f in files) {
   seps <- gregexpr('/',f)[[1]] # get all the '/' locations
   aftersep <- substr(f,seps[length(seps)]+1,nchar(f)) # get everything after the last '/'
   expname <- substr(aftersep,0,regexpr('_',aftersep)-1)
   compname <- substr(f,regexpr('_',f)+1,nchar(f))
   fileName.base <- paste(expname,substr(compname, 0, regexpr('.csv', compname)-1),sep='-')
-print(fileName.base)
+
+if (grepl('/', fileName.base)) {
+  library(stringr)
+  fileName.base <- str_replace_all(fileName.base, '/', '_')
+}
+cat(paste(fileName.base,"\n"))
+
   dat <- read.csv(f)
 
   dat.tmp <- dat
   dat.tmp[is.na(dat.tmp)] <- 0 # replace NAs with zeros
-  dat.ma <- apply(dat.tmp[,2:ncol(dat.tmp)], 2, ma.cent, n=181)
+  if (nrow(dat.tmp) < ma.window) {
+    ma.window.new <- nrow(dat.tmp)/4
+    cat("WARNING! MA Window of",ma.window,"is longer than series. Using window of",ma.window.new,"\n")
+    ma.window <- ma.window.new
+  }
+  dat.ma <- apply(dat.tmp[,2:ncol(dat.tmp)], 2, ma.cent, n=ma.window)
   dat.ma <- cbind(dat.tmp[,1], dat.ma)
   dat.ma <- as.data.frame(dat.ma)
   colnames(dat.ma) <- colnames(dat)
@@ -45,15 +69,33 @@ print(fileName.base)
     fileName <- paste("graphics/", fileName.base, "-", column,
                       ifelse(plot.data, "-wd", ""), sep="")
     if (plot.svg) {
-      svg(paste(fileName,".svg",sep=""), width=10, height=10)
+      svg(paste(fileName,".svg",sep=""), width=9, height=9)
+      cex=1
     } else {
       png(paste(fileName,".png",sep=""), width=1600, height=1600)
+      cex=2.5
+    }
+    par(cex=cex, lwd=3, mar=c(5,6,4,2), cex.main=1, cex.axis=1, cex.lab=1)
+
+    ## raw ≡ only raw data
+    ## nodata ≡ only moving average
+    ## data ≡ raw data + moving average
+    if (data.status == "data" || data.status == "raw") {
+      refData <- dat[[column]]
+      plot.data <- T
+    } else { # data.status == "nodata"
+      refData <- dat.ma
+      plot.data <- F
     }
 
-    par(cex=2, lwd=3, mar=c(5,6,4,2), cex.main=1, cex.axis=1, cex.lab=1)
-
-    plot(Time, dat.ma[[column]], ylab=column, type="l")
-    if (plot.data) points(dat[[column]], pch="·")
+    if (plot.data) {
+      plot( dat[ (row(dat)%%sample.freq)==0, 1], dat[ (row(dat)%%sample.freq)==0, column],
+           xlab=colnames(dat)[1], ylab=column, type="p", pch="·")
+      if (data.status == "data")
+        lines(dat.ma[ (row(dat.ma)%%sample.freq)==0, 1], dat.ma[ (row(dat.ma)%%sample.freq)==0, column], lwd=5)
+    } else {
+      plot(Time, dat.ma[[column]], ylab=column, type="l")
+    }
 
     grid()
     minor.tick(nx=5, ny=5, tick.ratio=0.5)
